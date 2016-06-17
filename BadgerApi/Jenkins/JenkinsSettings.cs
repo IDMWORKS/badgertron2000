5a2d9bd9b0e6b2feb201777cd5905cffb408a59c
Nate Woolls <nathanial.woolls@idmworks.com>
2016-06-17 16:46:37 -0400

    Change JenkinsSettings.Host to JenkinsSettings.RootURL so we can specify https & port if needed

--- BadgerApi/Jenkins/JenkinsSettings.cs
+++ BadgerApi/Jenkins/JenkinsSettings.cs
@@ -2,7 +2,7 @@ namespace BadgerApi.Jenkins
 {
     public class JenkinsSettings
     {
-        public string Host { get; set; }
+        public string RootURL { get; set; }
         public string Username { get; set; }
         public string Token { get; set; }
     }
