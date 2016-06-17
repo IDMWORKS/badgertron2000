274582a05ee7fdfedfeb2b23c3e2b6b59a3b6576
Nate Woolls <nathanial.woolls@idmworks.com>
2016-06-17 17:17:31 -0400

    Allow overriding default listening URLs

--- BadgerApi/Program.cs
+++ BadgerApi/Program.cs
@@ -1,6 +1,7 @@
 using System.IO;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Configuration;
 
 namespace BadgerApi
 {
@@ -8,12 +9,22 @@ namespace BadgerApi
     {
         public static void Main(string[] args)
         {
+            // allow overriding default listening URLs
+            // see https://github.com/aspnet/Announcements/issues/174
+            var config = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("hosting.json", optional: true)
+                .AddEnvironmentVariables(prefix: "ASPNETCORE_")
+                .AddCommandLine(args)
+                .Build();
+
             var host = new WebHostBuilder()
-                        .UseKestrel()
-                        .UseContentRoot(Directory.GetCurrentDirectory())
-                        .UseIISIntegration()
-                        .UseStartup<Startup>()
-                        .Build();
+                .UseConfiguration(config)
+                .UseKestrel()
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                .UseIISIntegration()
+                .UseStartup<Startup>()
+                .Build();
 
             host.Run();
         }
